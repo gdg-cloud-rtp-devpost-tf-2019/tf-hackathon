c203fde799ac4bcabcb7c1f722d68bc65d11e36b
John F. Davis <davisjf@gmail.com>
2019-04-03 15:14:48 -0400

    figured out station id problem with Tuthill.  Added gmaps capability to notebooks

--- src/software-to-install.sh
+++ src/software-to-install.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 conda install gdal -c conda-forge
+conda install -c conda-forge gmaps
+
+# do this inside of the tf env
+jupyter nbextension enable --py --sys-prefix widgetsnbextension
+jupyter nbextension enable --py --sys-prefix gmaps
+
+# Notes on how to get a google maps api kiey
+# https://jupyter-gmaps.readthedocs.io/en/latest/authentication.html
 
 
 
