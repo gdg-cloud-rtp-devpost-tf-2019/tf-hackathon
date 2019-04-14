367d416df93b8cb78ab6baa49f84004602432195
John F. Davis <davisjf@gmail.com>
2019-04-14 08:30:59 -0400

    add blank setenv.sh

---
+++ bin/setenv.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# To use this for setting your environment variable do this:
+# $ . ./setenv.sh
+
+# The .gitignore file should have an entry for this script so 
+# that you do not upload your key.  Do not remove it.
+
+# for gmaps api in juypter notebooks
+export GOOGLE_MAPS_API_KEY="Add your change here"
+
