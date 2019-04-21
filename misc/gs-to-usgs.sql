49d2be27e22e6a4965f6477aab633e53eb608cb2
John F. Davis <davisjf@gmail.com>
2019-04-21 17:00:41 -0400

    wip

---
+++ misc/gs-to-usgs.sql
@@ -0,0 +1,18 @@
+  /* water level = offset - distance 
+     Might need to pick max and min, but not sure since data is not correct
+     divide diff by 30.48 to convert cm to feet.
+   */
+SELECT
+  "GS" AS usgs,
+  336700001 AS site_no,
+  DATE(timestamp) AS date,
+  (MAX(offset) - MAX(distance))/ 30.48 AS tidal_high,
+  (MIN(offset) - MIN(distance))/ 30.48 AS tidal_low,
+  0.0 AS precipitation_inches,
+  AVG(lat) AS lat,
+  AVG(long) AS long
+FROM
+  `tf-hackathon-236400.greenstream.GSVBAP000001`
+GROUP BY
+  date
+
